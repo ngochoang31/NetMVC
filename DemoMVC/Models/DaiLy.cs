4add98995ac6da121c132f90c80a5ecbf8571c33
ngochoang31 <free31303@gmail.com>
2025-08-26 21:59:23 +0700

    Tao4classchoModels

---
+++ DemoMVC/Models/DaiLy.cs
@@ -0,0 +1,12 @@
+namespace DemoMVC.Models
+{
+    public class DaiLy
+    {
+        public string MaDaily { get; set; }
+        public string TenDaily { get; set; }
+        public string DiaChi { get; set; }
+        public string DienThoai { get; set; }
+        public string NguoiDaiDien { get; set; }
+        public string MaHTPP { get; set; }  
+    }
+}
